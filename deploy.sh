292a6d1bc83a5cdb7686e4fb17b27e3b5d66615c
yuxguo <yuxuanguo1999@gmail.com>
2021-03-26 17:55:50 +0800

    add deploy.scripts

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+rm -rf docs/
+hugo -t hugo-coder
+
+git add *
+
+msg="The site has been updated at $(date)."
+
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+printf "commit message is: \033[0;32m$msg\033[0m\n"
+
+git commit -m "$msg"
+
+git push origin master
